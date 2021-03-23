b411536d71e633c0c5be66582a0ca027be47699a
Lilin Lao <lll9p.china@gmail.com>
2021-03-23 10:44:48 +0800

    remove svn commands

--- scripts/01install_plugins.sh
+++ scripts/01install_plugins.sh
@@ -1,14 +1,4 @@
 #!/usr/bin/env bash
 echo 'Install plugins.'
-mkdir pelican-plugins
-svn checkout https://github.com/ingwinlu/pelican-bootstrapify/trunk/ pelican-plugins/bootstrapify
-svn checkout https://github.com/getpelican/pelican-plugins/trunk/tipue_search pelican-plugins/tipue_search
-svn checkout https://github.com/getpelican/pelican-plugins/trunk/better_codeblock_line_numbering pelican-plugins/better_codeblock_line_numbering
-svn checkout https://github.com/getpelican/pelican-plugins/trunk/always_modified pelican-plugins/always_modified
-svn checkout https://github.com/getpelican/pelican-plugins/trunk/gzip_cache pelican-plugins/gzip_cache
-svn checkout https://github.com/getpelican/pelican-plugins/trunk/neighbors pelican-plugins/neighbors
-svn checkout https://github.com/getpelican/pelican-plugins/trunk/render_math pelican-plugins/render_math
-svn checkout https://github.com/getpelican/pelican-plugins/trunk/series pelican-plugins/series
-svn checkout https://github.com/getpelican/pelican-plugins/trunk/post_stats pelican-plugins/post_stats
-svn checkout https://github.com/getpelican/pelican-plugins/trunk/sitemap pelican-plugins/sitemap
-svn checkout https://github.com/getpelican/pelican-plugins/trunk/related_posts pelican-plugins/related_posts
+git clone --depth=1 https://github.com/getpelican/pelican-plugins.git
+git clone --depth=1 https://github.com/ingwinlu/pelican-bootstrapify.git pelican-plugins/bootstrapify
