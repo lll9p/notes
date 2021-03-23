e67ee8485b78355b36d7e7ea27f7f2a9b6b779be
Lilin Lao <lll9p.china@gmail.com>
2021-03-23 10:50:24 +0800

    remove svn commands

--- scripts/01install_plugins.sh
+++ scripts/01install_plugins.sh
@@ -1,4 +1,4 @@
 #!/usr/bin/env bash
 echo 'Install plugins.'
 git clone --depth=1 https://github.com/getpelican/pelican-plugins.git
-git clone --depth=1 https://github.com/ingwinlu/pelican-bootstrapify.git pelican-plugins/bootstrapify
+( cd pelican-plugins && git submodule update --init bootstrapify )
